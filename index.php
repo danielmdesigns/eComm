9f5d4b8cd9925f984dae4cc47df2f028b7d4a6b1
danielmdesigns <danielwmaldonado@gmail.com>
2015-04-29 08:08:51 -0500

    initial commit
    original index.php for eComm

---
+++ index.php
@@ -0,0 +1,212 @@
+<?
+    $page = "index"; 
+    include_once("../../Documents/eComm Website/includes/header.php");
+    include_once("../../Documents/eComm Website/includes/nav.php");
+?>
+
+<div class="outer" id="hdr">
+  <div class="inner">
+    <img src="/img/header/slider-2.png"/>
+    <div class="desc">
+      <h1>Get the real estate<br> commissions <br>
+
+<strong> you've earned,</strong></h1>
+<h2> without the wait!</h2>
+   
+
+
+     <div id="grow" class="hidden-xs">
+      <p><strong>• No credit check</strong> • Fast and Easy 
+      </p></div>
+      
+      <a class="joinButton btn orange btn-lg">Let's Get Started<small>Apply in minutes. </small></a>
+      
+        <!-- explainer video with thumbnail-->
+      <div class="hpg-vid hidden-xs hidden-sm">
+    
+<a href="//fast.wistia.net/embed/iframe/otfsmkbvgv?popover=true" class="wistia-popover[height=360,playerColor=cf6e23,width=640]"><img src="https://embed-ssl.wistia.com/deliveries/21a08fd263dc2d1c6dceb777de74e42aa940093c.jpg?image_play_button=true&image_play_button_color=cf6e23e0&image_crop_resized=180x101" alt="" /></a>
+<script charset="ISO-8859-1" src="//fast.wistia.com/assets/external/popover-v1.js"></script>
+    
+    
+      </div>
+  <!--  end explainer with thumbnail-->  
+  
+  <!-- start vid with text-->
+     <div class="vid-txt hidden-md hidden-lg"> <br />
+     <p class="center">
+    <a href="//fast.wistia.net/embed/iframe/otfsmkbvgv?popover=true" class="wistia-popover[height=360,playerColor=cf6e23,width=640]">
+    
+    <span class="glyphicon glyphicon-play-circle" style="font-size:20px;"></span> See How It Works</a>
+<script charset="ISO-8859-1" src="//fast.wistia.com/assets/external/popover-v1.js"></script>
+</p>
+     </div>
+  <!-- end vid with text-->   
+  
+    </div><!-- end .desc -->
+    <div class="ftr">
+      <div class="pull-right">
+        <img src="/img/logos/BBB-logo.png"> <img src="/img/logos/truste-logo.png" onclick="verifySeal();">
+      </div><!-- end .pull-right -->
+      <strong>We’re here to help:</strong><br/>
+      <img src="/img/icons/phone-small.png" width="29" height="24" alt=""/>Call us at <span><a href="tel:+18778824368" style="text-decoration:none;">1-877-882-4368</a>
+      </span> or <img src="/img/icons/chat-small.png" width="29" height="24" alt=""/><a onclick="javascript:OpenLHNChat();return false;" href="#">Click to Chat</a>
+    </div><!-- end .ftr -->
+  </div><!-- end .inner -->
+</div><!-- end #hdr -->
+
+<div class="outer" id="content">
+  <div class="inner">
+    <h2 class="center">Commission Advances for 
+<br/>
+      <strong>Real Estate 
+Sales Professionals</strong></h2>
+    <br/>
+    <p class="center"><strong>eCommission</strong> is a service that lets you advance your real estate commissions before closing. It’s not a loan. It’s simply access to the money you’ve earned, whenever you want it. Enjoy greater <strong>control, choice</strong> and <strong>flexibility</strong> over your business cash flow.
+ </p>
+    <br/>
+    <h3 class="center">Receive up to <br>
+$15,000 
+      <strong>today!</strong></h3><br>
+
+  </div><!-- end .inner -->
+</div><!-- end outer #content -->
+
+<!-- tab section start-->
+<div class="outer" id="content">
+  <div class="inner lt-teal">
+  
+    <hr class="hidden"/>
+    <div class="tabWrapper">
+      <ul class="tabs">
+        <li  class="active"><a href="#tab1" class="">What to Expect</a></li>
+        <li><a href="#tab2" class="">What It Costs</a></li>
+        <li><a href="#tab3" >What if the Sale Cancels?</a></li>
+      </ul>
+      <div class="panes">
+        <div class="pane active" id="tab1">
+          <div class="row">
+            <div class="col-sm-5" id="hpgslider">
+              <p>From start to finish, the <strong>eCommission</strong> process for advancing your commissions is fast and easy. </p>
+            
+              <ul style="text-align:left;">
+                <li>Apply online in minutes</li>
+                <li>Receive  agreements to electronically sign</li>
+                <li>Your sale information is confirmed</li>
+                 <li>Funds wired within 1 business day!</li>
+              </ul>
+           
+              <br/>
+              <p class="center" style="margin-bottom: 15px;"> 
+            
+              <a class="joinButton btn orange btn-lg"><strong>Let's Get Started</strong><br/>
+          <small>No credit check.</small></a></p>
+           
+           
+              </div>
+          
+
+
+            
+            <br>
+            <div class="col-sm-7">
+              <img src="/img/page/laptop.png" class="responsive"/>
+            </div>
+          </div>
+          
+<div class="offer" style="margin-top:-50px;"><strong>NEW CUSTOMER BONUS</strong> Receive 30% off your fee! Use code NEW30</div>
+
+ <br />
+ &nbsp;
+ <br />
+ <br /> <br />
+        </div><!-- end #tab1 -->
+        <div class="pane" id="tab2">
+     
+          <h2 class="center">Commission Advance Estimator<br/>
+            <strong>Get up to $15,000</strong></h2>
+          <h3 class="center">Slide to select your commission advance</h3>
+          <div class="slider" id="commission">
+          </div><!-- end .#commission -->
+          
+          <div class="row">
+            <div class="col-sm-6">
+              <table cellpadding=0 cellspacing=0 border=0>
+                <tr class="advance">
+                  <td>Advance Amount</td>
+                  <td><div id="sliderValue">$0</div></td>
+                </tr>
+                <tr class="underline">
+                  <td>Fee</td>
+                  <td><div id="feesValue">$750</div></td>
+                </tr>
+                <tr class="total">
+                  <td>Due At Closing</td>
+                  <td><div id="totalValue">$8,250</div></td>
+                </tr>
+              </table>
+            </div><!-- end .col-sm-6 -->
+            <div class="col-sm-6 btnContainer" id="hpgslider">
+            <p class="center"><br>
+
+              <a class="joinButton btn orange btn-lg">Let's Get Started<br><small>No hidden fees. No credit check.</small></a></p>
+              
+            <p class="center small">  Your fees are tax deductible!</p>
+            </div><!-- end .col-sm-6 btnContainer -->
+             <div class="col-sm-12">
+             <br>
+             <br>
+               <h3 class="center">The cost of an <strong>eCommission</strong> advance
+               </h3> <br>
+               <ul>
+               <li>Example is based on a 40 day closing. Longer advances available up to 120 days.</li>
+               <li>The fee is added to your advance, meaning you pay nothing upfront to use this service.</li>
+               <li>There is a 20-day grace period from the closing date to have the advance repaid without any additional cost.</li>
+               <li>Advance limits may vary based on sales production.</li>
+               
+                       
+               </ul>
+ <br />
+  <div class="offer"><strong>NEW CUSTOMER BONUS </strong> Receive 30% off your fee! Use code NEW30<br>
+ </div>
+<br><br>
+
+               
+            </div>
+          </div><!-- end .row -->
+        </div><!-- end #tab2 -->
+        
+        <div class="pane" id="tab3">
+          
+          
+	<div class="row" id="hpgslider">
+		
+        <p class="center">
+<img src="/img/page/90percent.png" height="100" width="300" class="responsive">
+</p>
+    
+             
+              <p class="center">On average, 90% of single family residential sales close as expected. In those rare instances when a sale falls through, simply replace the advance using future earned commissions. </p><br>
+
+		
+                   <p class="center">
+              <a class="joinButton btn orange btn-lg">Let's Get Started<br><small>Apply in minutes. No credit check.</small></a></p>
+          
+           <p class="center small">  Your fees are tax deductible!</p><br>
+<br>
+
+		
+	</div>
+          
+          
+        </div><!-- end #tab3 -->
+      </div><!-- end .panes -->
+    </div><!-- end .tabWrapper -->
+  </div><!-- end .inner -->
+</div><!-- end #content -->
+<!-- tab section end-->
+
+<?
+    include_once("../../Documents/eComm Website/includes/slider.php");
+    include_once("../../Documents/eComm Website/includes/videos.php");
+    include_once("../../Documents/eComm Website/includes/footer.php");
+?>
