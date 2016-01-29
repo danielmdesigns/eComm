e98f96387ceba757b6e34c1b4af329c649b2bf64
Daniel Maldonado <danielwmaldonado@gmail.com>
2016-01-29 14:31:56 -0600

    initial commit
    old broker.php page

---
+++ old-broker.php
@@ -0,0 +1,119 @@
+<?
+    $page = "broker"; 
+    include_once("includes/header.php");
+    include_once("includes/nav.php");
+?>
+
+<style>
+/*#submenu {
+	margin-top:87px;
+}
+@media (max-width:850px){
+#submenu {
+	margin-top:10px;
+}
+#hdr2 {
+	margin-top:0;
+	border: 1px solid red;
+}	
+}*/
+</style>
+
+<div class="outer" id="hdr2">
+  <div class="inner">
+    <img src="/img/header/broker-header.jpg"/>
+    <div class="desc">
+      <h2>Broker <strong>FAQ's</strong></h2>
+    </div>
+    <div class="ftr">
+      <div class="pull-right">
+        <img src="/img/logos/BBB-logo.png"> <img src="/img/logos/truste-logo.png">
+      </div>
+      <strong>We’re here to help:</strong><br/>Call us at <span>1-877-882-4368</span> or <a onclick="javascript:OpenLHNChat();return false;" href="#">Click to Chat</a>
+    </div>
+  </div>
+</div>
+
+<div class="outer spacer" id="content">
+  <div class="inner lt-teal">
+    <div class="row ">
+    
+        <p class="center">
+     <strong>eCommission is a powerful tool for recruiting new agents and retaining your top performers.  Give your agents the financial control that comes with  receiving advance payment of their commissions - with no risk or broker liability.</strong>
+     	</p>
+
+    </div>
+  </div>
+</div>
+
+<div class="outer spacer" id="content">
+  <div class="inner ">
+    <div class="row wider big spacer"> 
+  
+<div class="col-sm-1"></div><!-- end .col-sm-1 -->
+<div class="col-sm-10">
+    <p class="center">Here are some important things Brokers should know about our service:</p>
+
+  <br/>
+        
+  <h2 class="center">Who is <strong>eCommission</strong>?</h2>
+  <p class="center "><br/>
+  <strong>eCommission</strong> is the <strong># 1 provider</strong> of commission advance services to real estate professionals in the United States, having advanced nearly $1 BILLION since 1999. The Management Team of <strong>eCommission</strong> pioneered the service of advancing real estate commissions. Our single-minded focus has been to develop financial products and services dedicated to helping real estate professionals better manage their business cash flow.</p>
+  
+  <br/>
+  
+  <h2 class="center">What is a <strong>Commission Advance</strong>?</h2>
+  <p class="center "><br/>
+  A commission advance is a financial service whereby an agent and broker sell a portion of a pending commission for a fee. In exchange, funds are advanced before closing. It’s not a loan. It’s simply access to an earned commission prior to an estimated settlement date. </p>
+  
+  <br>
+  
+  <h2 class="center">Why Does the Broker sign the <strong><br>
+  Advance Agreement</strong>?</h2>
+  <p class="center "><br/>
+  As broker, all commissions earned by your agents belong to you. We cannot advance funds without your consent. The agreement you sign with <strong>eCommission</strong> authorizes the sale and assignment of the agent’s portion of the commission on a particular sale, after any split with you, to <strong>eCommission</strong>.</p>
+  
+  <br>
+  
+  <h2 class="center">Does the Broker Have Any Risk or Liability?</h2>
+  <p class="center "><br/>
+  <strong>No.</strong> In the unlikely event we advance a commission to one of your agents on a sale that falls through, it is the agent’s responsibility to replace it using future earned commissions. As broker, your only responsibility is to direct any future commissions earned by the agent to <strong>eCommission</strong> in the event the advance is not repaid as agreed. If the agent has no future commissions or leaves your company for example, there is never any liability on your part to repay their advance.</p>
+  
+  <br>
+  
+  <h2 class="center">How is the Advance <strong>Repaid</strong>?</h2>
+  <p class="center "><br/>
+  At the time an agent applies for an <strong>eCommission</strong> advance, the broker is asked to sign a commission disbursement authorization to the settlement company. This document is your instruction to send the portion of the agent’s commission that has been advanced, directly to <strong>eCommission</strong> at close of escrow. 
+  </p>
+  
+  <br>
+  
+  <h2 class="center">What are the Advance Limits? </h2>
+  <p class="center "><br/>
+  <strong>eCommission</strong> provides commission advances up to 120 days ahead of the scheduled closing date.  An agent can request up to 100% of their net earned commission after any split with their broker, to a maximum of $15,000 per transaction. These advance limits can be lowered at your discretion, just give us a call to discuss. </p>
+  
+  <br>
+  
+  <h2 class="center">Why is this <strong>good</strong> for my <strong>agents</strong>? </h2>
+  <p class="center "><br/>
+  Better control of cash flow allows agents to manage their professional lives more efficiently.  Experience indicates that agents typically use <strong>eCommission</strong> 2 to 3 times per year, whenever business expenses surpass monthly cash flow.   With <strong>eCommission</strong> as your partner, you can help your agents better control their cash flow keeping them focused and engaged, resulting in more listings, more sales and higher levels of productivity.</p>
+  
+  <br>
+  
+  <h2 class="center">Does <strong>eCommission</strong> Advance Commissions to <strong>Broker Agents</strong>?</h2>
+  <p class="center "><br/>
+  <strong>Yes.</strong> <strong>eCommission</strong> will advance to brokers who are also buying / selling agents on their own sales. This is done in conjunction with our subsidiary 3Click Advance.  There are a few restrictions on the type and size of advances we can provide, however.</p>
+  
+  </div><!-- end .col-sm-10 -->      
+<div class="col-sm-1"></div><!-- end .col-sm-1 -->
+      
+    </div><!-- end .row wider big spacer -->
+  </div><!-- end .inner -->
+</div><!-- end #content -->
+
+<div class="outer spacer" id="commissionWrapper">
+<?
+   include_once("includes/slider.php");
+/*     include_once("includes/videos.php"); */
+    include_once("includes/footer.php");
+?>
